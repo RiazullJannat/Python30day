ada21a848546ac59b42dc9246c495354103da1d6
RJannat <ramimk097@gmail.com>
2024-05-02 06:49:21 -0400

    Learn print function,comment in python,variables,type casting

---
+++ firstprogram.py
@@ -0,0 +1,95 @@
+#print("Hello world!")
+#comments in python
+#print("Hellow world!") #Hello
+# print("Hello world!")
+# print("Hello world!")
+# print("Hello world!")
+# print("Hello world!")
+# print("Hello world!")
+# print("Hello world!")
+# print("Hello world!")
+"""This
+is
+ a
+  multi
+   line
+    comment
+    """
+#variables in python
+"""Variables are use for store data in python."""
+a=3
+print(a)
+b=44
+print(b)
+c=4.4
+print(c)
+d=True #or False this are boolen type value
+print(d)
+e="RJannat"
+print(e)
+f=None #None type value
+print(f)
+
+
+#Type casting
+g="string"
+h=1 #int
+i=1.1 #flot
+j=True or False #Boolen
+
+ga=7
+print(type(ga))
+gb=3.0
+print(type(gb))
+gc=ga+gb
+print(gc)
+print(type(gc))
+gd=ga*gb
+print(gd)
+print(type(gd))
+#casting int to float
+i_n=5
+n=float(i_n)
+print(n)
+print(type(n))
+
+#casting float to int
+fn=2.9
+i_n_t=int(fn)
+print(i_n_t)
+print(type(i_n_t))
+
+#casting int to str
+sn=5
+nns=str(sn)
+print(nns)
+print(type(nns))
+
+#casting str to float
+fnn="5.6"
+nnn=float(fnn)
+print(nnn)
+print(type(nnn))
+
+'''a = "5"
+b = 't'
+
+# typecast to int
+n = int(a)
+
+print(n)
+print(type(n))
+
+print(int(b))
+print(type(b))'''
+
+'''# integer variable
+a = 5
+# string variable
+b = 't'
+
+# typecast to int
+n = a + b
+
+print(n)
+print(type(n))'''
